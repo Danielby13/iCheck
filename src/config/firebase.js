5af1c4c84b72fff674746b01787a26a89c1c0b78
Danielby13 <danielby13@gmail.com>
2021-05-31 18:57:22 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,9 @@
-export default {
-  apiKey: "AIzaSyCX34EKvzzzM2dd8xEEpzy0MW2SUdc0iF8",
-  authDomain: "icheck-fad87.firebaseapp.com",
-  projectId: "icheck-fad87",
-  storageBucket: "icheck-fad87.appspot.com",
-  messagingSenderId: "161608127979",
-  appId: "1:161608127979:web:622be3d9070ff79cb4b5ae",
-  measurementId: "G-CRNZNHY9RQ",
+export default { // Censored due to sensitive information
+  apiKey: "******",
+  authDomain: "******",
+  projectId: "******",,
+  storageBucket: "******",
+  messagingSenderId: "******",
+  appId: "********",
+  measurementId: "******",
 };
